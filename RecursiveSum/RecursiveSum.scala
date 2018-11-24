edce92ef9a3caca34d9496dd36483ad9d3a923d7
Hagos Salih <hsalih@box.com>
2018-11-24 14:24:17 -0800

    Modified argument passing to range

--- RecursiveSum/RecursiveSum.scala
+++ RecursiveSum/RecursiveSum.scala
@@ -1,5 +1,12 @@
 object RecursiveSum extends App {
-  val intList:List[Int] = args.toList.map(_.toInt)
+  /*
+  A couple of options for passing the List of numbers
+	1. If the list is known in advance replace the `intList` variable with the following value
+  	val intList:List[Int] = args.toList.map(_.toInt)
+	2. If the List is a range of values use the program as is. 
+  */
+  val intList:List[Int] = List.range(args(0).toInt, args(1).toInt)
+  //The sum function below can result in StackOverflow if range is too big
   def sum(ints: List[Int]): Int = {
       ints match {
         case Nil => 0
--- RecursiveSumWithAccumulator/RecursiveSumWithAccumulator.scala
+++ RecursiveSumWithAccumulator/RecursiveSumWithAccumulator.scala
@@ -1,5 +1,11 @@
 object RecursiveSumWithAccumulator extends App {
-  val intList:List[Int] = args.toList.map(_.toInt)
+  /*
+  A couple of options for passing the List of numbers
+        1. If the list is known in advance replace the `intList` variable with the following value
+        val intList:List[Int] = args.toList.map(_.toInt)
+        2. If the List is a range of values use the program as is.
+  */
+  val intList:List[Int] = List.range(args(0).toInt, args(1).toInt)
   def sum(ints: List[Int]): Int = {
     def sumAccumulator(ints: List[Int], accum: Int): Int = {
       ints match {
